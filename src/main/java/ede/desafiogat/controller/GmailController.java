f0557d323ce890db3218adecbc0fcdeed16c2c4e
éde <edemarinho@outlook.com>
2021-10-21 16:05:20 -0300

    Implementa os métodos que recuperam os emails

--- src/main/java/ede/desafiogat/controller/GmailController.java
+++ src/main/java/ede/desafiogat/controller/GmailController.java
@@ -1,19 +1,31 @@
 package ede.desafiogat.controller;
 
+import ede.desafiogat.gmail.service.GmailService;
+import ede.desafiogat.models.Email;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/")
-@AllArgsConstructor
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class GmailController {
 
-    @PostMapping("/Callback")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void GetToken (@RequestBody String data){
-        System.out.println(data);
-    }
+    private GmailService gmailService;
 
+    @GetMapping("/getmails")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void GetMails () throws IOException, GeneralSecurityException {
+
+        List<Email> emails = gmailService.processMessages();
+        for (Email email : emails){
+            System.out.println(email);
+        }
+    }
 }
